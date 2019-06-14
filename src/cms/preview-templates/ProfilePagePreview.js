f220b92b6f1b49c58381e80d51006887327666e1
sreng sun <sreng@infinity.com.kh>
2019-06-14 16:39:32 +0700

    ok

---
+++ src/cms/preview-templates/ProfilePagePreview.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ProfilePageTemplate } from '../../templates/profile-page';
+
+const ProfilePagePreview = ({ entry, widgetFor }) => (
+  <ProfilePageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+);
+
+ProfilePagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func
+  }),
+  widgetFor: PropTypes.func
+};
+
+export default ProfilePagePreview;
